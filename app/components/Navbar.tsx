f0e963b69d913bc39b4281ff209d100ea8a824bb
mohammad-jf <m.h.jalifathi@gmail.com>
2025-11-27 04:31:23 +0330

    dropdown menu,hero section

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,7 +1,10 @@
-import { FaLaptopCode } from "react-icons/fa";
+import { FaLaptopCode, FaTimes, FaBars } from "react-icons/fa";
 import { NavLink } from "react-router";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
     <nav className="bg-gray-800 border-b border-gray-700 shadow-md sticky top-0 z-50">
       <div className="max-w-6xl mx-auto px-6 py-4 flex justify-between items-center">
@@ -73,7 +76,83 @@ const Navbar = () => {
             </NavLink>
           </div>
         </div>
+
+        {/* hamburger menu */}
+        <div className="flex items-center gap-4 md:hidden">
+          <button
+            className="text-blue-400 text-xl cursor-pointer"
+            title="menu"
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+          >
+            {isMenuOpen ? <FaTimes /> : <FaBars />}
+          </button>
+        </div>
       </div>
+
+      {/* mobile nav */}
+      {isMenuOpen && (
+        <div className="bg-gray-800 border-t border-gray-700 px-6 py-4 space-y-2 space-x-4 text-center md:hidden">
+          <NavLink
+            to="/"
+            onClick={() => setIsMenuOpen(false)}
+            className={({ isActive }) =>
+              isActive
+                ? "text-blue-300 font-bold"
+                : "text-gray-300 hover:text-blue-300 transition-colors duration-300"
+            }
+          >
+            Home
+          </NavLink>
+
+          <NavLink
+            to="/projects"
+            onClick={() => setIsMenuOpen(false)}
+            className={({ isActive }) =>
+              isActive
+                ? "text-blue-300 font-bold"
+                : "text-gray-300 hover:text-blue-300 transition-colors duration-300"
+            }
+          >
+            Projects
+          </NavLink>
+
+          <NavLink
+            to="/blogs"
+            onClick={() => setIsMenuOpen(false)}
+            className={({ isActive }) =>
+              isActive
+                ? "text-blue-300 font-bold"
+                : "text-gray-300 hover:text-blue-300 transition-colors duration-300"
+            }
+          >
+            Blogs
+          </NavLink>
+
+          <NavLink
+            to="/about"
+            onClick={() => setIsMenuOpen(false)}
+            className={({ isActive }) =>
+              isActive
+                ? "text-blue-300 font-bold"
+                : "text-gray-300 hover:text-blue-300 transition-colors duration-300"
+            }
+          >
+            About
+          </NavLink>
+
+          <NavLink
+            to="/contact"
+            onClick={() => setIsMenuOpen(false)}
+            className={({ isActive }) =>
+              isActive
+                ? "text-blue-300 font-bold"
+                : "text-gray-300 hover:text-blue-300 transition-colors duration-300"
+            }
+          >
+            Contact
+          </NavLink>
+        </div>
+      )}
     </nav>
   );
 };
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,3 +1,4 @@
+import Hero from "~/components/Hero";
 import type { Route } from "./+types/index";
 
 export function meta({}: Route.MetaArgs) {
@@ -7,9 +8,9 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 export default function Home() {
-  console.log("hello from home");
   return (
     <section>
+      <Hero name="mohammad" />
     </section>
   );
 }
